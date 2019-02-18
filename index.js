5b75a7273e8d116822ac50dd482f7d052d8398cc
Bipul Ranjan <bipul.ranjan@auw.co.in>
2019-02-19 04:09:28 +0530

    HTTPS support

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const https = require("https"),
+  fs = require("fs");
 var cors = require('cors');
 var routes = require('./app/routes');
 var jwtAuth = require('./app/middlware/jwtAuth');
@@ -42,3 +44,11 @@ app.use('/',routes);
 
 // Start express app
 app.listen(process.env.APP_PORT, () => console.log(`App started on port ${process.env.APP_PORT}`))
+
+if(process.env.APP_SECURE!="false"){
+    const options = {
+        key: fs.readFileSync(process.env.APP_KEY),
+        cert: fs.readFileSync(process.env.APP_CERT)
+      };
+    https.createServer(options, app).listen(process.env.APP_SECURE_PORT);
+}
